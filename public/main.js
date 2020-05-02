257a6bc2ae9744fd1d40b725ea5172f097767f34
Geon George <geongeorge98@gmail.com>
2020-05-02 23:31:34 +0530

    Edited demo

--- public/main.js
+++ public/main.js
@@ -7,6 +7,9 @@ var txt =
   'Lorem ipsum dolor sit ame Lorem ipsum dolor sit ame Lorem ipsum dolor sit ame'
 
 canvasTxt.fontSize = 24
+canvasTxt.fontWeight ='100'
+canvasTxt.fontStyle = 'oblique'
+canvasTxt.fontVariant = 'small-caps'
 // canvasTxt.debug = true
 canvasTxt.align = 'center'
 canvasTxt.vAlign = 'middle'

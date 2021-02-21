4ef58c532e4a99660d7f3ca44ae5b34ad5a2d904
aditya <kumar.93@iitj.ac.in>
2021-02-21 11:52:39 +0530

    reset public/main

--- public/main.js
+++ public/main.js
@@ -4,14 +4,13 @@ var c = document.getElementById('myCanvas')
 var ctx = c.getContext('2d')
 
 var txt =
-  'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum'
+  'Lorem ipsum dolor sit ame Lorem ipsum dolor sit ame Lorem ipsum dolor sit ame'
 
-canvasTxt.fontSize = 40
+canvasTxt.fontSize = 24
 canvasTxt.fontWeight = '100'
 canvasTxt.fontStyle = 'oblique'
-canvasTxt.yLimit = 'top'
-// canvasTxt.fontVariant = 'small-caps'
-canvasTxt.debug = true
+canvasTxt.fontVariant = 'small-caps'
+// canvasTxt.debug = true
 canvasTxt.align = 'center'
 canvasTxt.vAlign = 'middle'
 // canvasTxt.justify = true

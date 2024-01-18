8ec6ec7ea05176b003444b616cfc6fd19da214ec
Stefan Cameron <stefan@stefcameron.com>
2024-01-18 15:32:34 -0600

    Temporarily remove node-canvas reference causing node-gyp failures

--- src/node-test.ts
+++ src/node-test.ts
@@ -1,7 +1,16 @@
-import { createCanvas } from 'canvas'
+// DEBUG import { createCanvas } from 'canvas'
 import { drawText } from './canvas-txt/index'
 // @ts-ignore
 import * as fs from 'fs'
+
+// DEBUG REMOVE once figure out how to include canvas@^2.11.2 in devDependencies
+//  and have `yarn install` actually succeed past the node-gyp process that keeps
+//  failing even after reinstalling XCode Command Line Tools
+// @ts-ignore
+const createCanvas = (x: number, y: number) => ({
+  // @ts-ignore
+  getContext(name: string) {}
+})
 
 function main() {
   const canvas = createCanvas(400, 400)
